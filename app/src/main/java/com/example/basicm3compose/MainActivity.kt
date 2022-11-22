d8e602a03c51cc524e36dc2e175c8dd2a806c57c
Dan Nizri <dniz@google.com>
2022-11-22 14:11:20 -0500

    Initial commit

---
+++ app/src/main/java/com/example/basicm3compose/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.basicm3compose
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.basicm3compose.ui.theme.BasicM3ComposeTheme
+import com.google.android.material.datepicker.MaterialDatePicker
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      BasicM3ComposeTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+          Greeting("Android")
+        }
+      }
+    }
+
+    MaterialDatePicker.Builder.datePicker()
+      .build()
+      .show(supportFragmentManager, "DatePicker")
+  }
+}
+
+@Composable
+fun Greeting(name: String) {
+  Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+  BasicM3ComposeTheme {
+    Greeting("Android")
+  }
+}
