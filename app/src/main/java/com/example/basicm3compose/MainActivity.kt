62f397c795113bef5651276d2d8752e256fe0f07
Dan Nizri <dniz@google.com>
2022-12-07 12:14:20 -0500

    Set up splash screen to reproduce button color issue

--- app/src/main/java/com/example/basicm3compose/MainActivity.kt
+++ app/src/main/java/com/example/basicm3compose/MainActivity.kt
@@ -10,12 +10,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.example.basicm3compose.ui.theme.BasicM3ComposeTheme
 import com.google.android.material.datepicker.MaterialDatePicker
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
+    // Both of the following are required to reproduce the issue
+    WindowCompat.setDecorFitsSystemWindows(window, false)
+    installSplashScreen()
+
     setContent {
       BasicM3ComposeTheme {
         // A surface container using the 'background' color from the theme
